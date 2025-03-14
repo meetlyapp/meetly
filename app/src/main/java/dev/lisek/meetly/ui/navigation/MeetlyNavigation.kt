3219b97aa9d3002b35ad89aa5f7869ca64c93eb8
timGalk <tivron@outlook.com>
2025-03-14 21:46:55 +0100

    First commit

---
+++ app/src/main/java/dev/lisek/meetly/ui/navigation/MeetlyNavigation.kt
@@ -0,0 +1,27 @@
+package dev.lisek.meetly.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dev.lisek.meetly.ui.PasswordReset
+
+@Composable
+fun MeetlyNavigation(
+    modifier: Modifier = Modifier,
+
+) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login",
+        builder = {
+            composable("PasswordReset") {
+                PasswordReset(navController)
+            }
+        }
+    )
+
+}
