dbc88488e57d4366a6ef37e4ebb5df9504d8d9b2
timGalk <tivron@outlook.com>
2025-04-18 16:36:23 +0200

    Add docstrings for GoogleSignInClient

--- app/src/main/java/dev/lisek/meetly/ui/sidebar/Sidebar.kt
+++ app/src/main/java/dev/lisek/meetly/ui/sidebar/Sidebar.kt
@@ -60,14 +60,14 @@ import androidx.navigation.compose.rememberNavController
 import coil3.compose.AsyncImage
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
-import dev.lisek.meetly.ui.homescreen.HomeScreen
-import dev.lisek.meetly.ui.homescreen.MeetingPanel
-import dev.lisek.meetly.ui.profile.Profile
-import dev.lisek.meetly.ui.theme.DarkOrange
 import dev.lisek.meetly.R
 import dev.lisek.meetly.ui.ListCategory
 import dev.lisek.meetly.ui.ListEntry
+import dev.lisek.meetly.ui.homescreen.HomeScreen
 import dev.lisek.meetly.ui.homescreen.MeetingEntry
+import dev.lisek.meetly.ui.homescreen.MeetingPanel
+import dev.lisek.meetly.ui.profile.Profile
+import dev.lisek.meetly.ui.theme.DarkOrange
 import dev.lisek.meetly.ui.theme.scriptFamily
 import kotlinx.coroutines.launch
 
@@ -231,13 +231,11 @@ fun Overlay(parent: NavController) {
                         ) ?: return@composable, false
                     )
                 }
-//                composable("ftf") { FaceToFace(innerPadding) }
                 composable(
                     "profile/{uid}"
                 ) { arg ->
                     Profile.FromUID(arg.arguments?.getString("uid")!!)
                 }
-//                composable("chatlist") { ChatSpace(innerPadding) }
                 composable("create") { MeetingPanel() }
             }
             IconButton(onClick = {
