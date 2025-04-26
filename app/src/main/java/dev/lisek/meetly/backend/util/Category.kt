0c954c27665a800eb8835438d8e8ea274d7f3320
Camille Lisek <camille@lisek.dev>
2025-04-27 00:50:03 +0200

    Connect the category buttons to the backend
    The buttons are now externed as an extension of
    MultiChoiceSegmentedButtonRowScope
    
    Clicking on them correctly toggles the state
    and filters over categories

--- app/src/main/java/dev/lisek/meetly/backend/util/Category.kt
+++ app/src/main/java/dev/lisek/meetly/backend/util/Category.kt
@@ -6,17 +6,11 @@ import androidx.compose.material.icons.filled.Celebration
 import androidx.compose.material.icons.filled.Dashboard
 import androidx.compose.material.icons.filled.Event
 import androidx.compose.material.icons.filled.Gamepad
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Landscape
 import androidx.compose.material.icons.filled.LocalBar
 import androidx.compose.material.icons.filled.Movie
 import androidx.compose.material.icons.filled.Radar
 import androidx.compose.material.icons.filled.SportsBasketball
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.compositeOver
-import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.graphics.vector.ImageVector
 
 /**
@@ -41,39 +35,4 @@ enum class Category(
     OUTDOOR_GAMES(Icons.Default.SportsBasketball, "Outdoor games"),
     SWIM(Icons.Default.BeachAccess, "Pool & beach"),
     VIDEO_GAMES(Icons.Default.Gamepad, "Video games");
-
-//    val color: Color
-//        @Composable get() = emojiToColor(emoji)
-//    val textColor: Color
-//        @Composable get() = textColor(color)
-
-    /**
-     * Color map the category from its emoji.
-     * 
-     * @param [emoji] emoji to convert.
-     * @return background color.
-     */
-    @Composable
-    fun emojiToColor(emoji: String): Color {
-        val hash = emoji.codePoints().sum()
-        val r = (hash * 31 % 256) / 255f
-        val g = (hash * 67 % 256) / 255f
-        val b = (hash * 101 % 256) / 255f
-        return Color(r, g, b, .1f)
-            .compositeOver(MaterialTheme.colorScheme.primary)
-    }
-
-    /**
-     * Determine the text color based on the background color.
-     * 
-     * @param [bg] background color.
-     * @return text color.
-     */
-    fun textColor(bg: Color): Color {
-        return if(bg.luminance() > .5) {
-            Color.Black
-        } else {
-            Color.White
-        }
-    }
 }
--- app/src/main/java/dev/lisek/meetly/ui/homescreen/HomeScreen.kt
+++ app/src/main/java/dev/lisek/meetly/ui/homescreen/HomeScreen.kt
@@ -13,22 +13,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MultiChoiceSegmentedButtonRow
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.SegmentedButton
-import androidx.compose.material3.SegmentedButtonColors
-import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -40,7 +34,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,8 +41,7 @@ import com.google.android.gms.maps.model.LatLng
 import dev.lisek.meetly.backend.data.FetchData
 import dev.lisek.meetly.backend.util.Category
 import dev.lisek.meetly.backend.data.entity.MeetingEntity
-import dev.lisek.meetly.ui.theme.DarkOrange
-import dev.lisek.meetly.ui.theme.LightOrange
+import dev.lisek.meetly.ui.CategoryButton
 
 /**
  * HomeScreen screen of the app - collective meetings.
@@ -65,14 +57,15 @@ fun HomeScreen(pad: PaddingValues = PaddingValues(0.dp)) {
     var refresher by remember { mutableStateOf(false) }
     var posts by remember { mutableStateOf(emptyList<MeetingEntity>()) }
     var location by remember { mutableStateOf("") }
+    var categories by remember { mutableStateOf(emptySet<String>()) }
     val context = LocalContext.current
 
     getCurrentLocation(context) {
         location = getAddressFromLatLng(context, it ?: LatLng(.0, .0)).split(",")[0]
     }
 
-    LaunchedEffect(refresher) {
-        posts = FetchData.fetchMeetings(context, radius)
+    LaunchedEffect(refresher, categories) {
+        posts = FetchData.fetchMeetings(context, radius, categories)
     }
 
     Column(modifier = Modifier.fillMaxSize().padding(pad)) {
@@ -113,30 +106,18 @@ fun HomeScreen(pad: PaddingValues = PaddingValues(0.dp)) {
                 }
             },
         )
-        LazyColumn(Modifier.weight(1f)) {
-            item {
-                MultiChoiceSegmentedButtonRow(Modifier.horizontalScroll(ScrollState(0))) {
-                    for (category in Category.entries) {
-                        SegmentedButton(
-                            true,
-                            {},
-                            RoundedCornerShape(100),
-                            Modifier.padding(4.dp, 0.dp),
-                            true,
-                            SegmentedButtonDefaults.colors(LightOrange, Color.Black, DarkOrange),
-                            icon = { Icon(category.icon, null) }
-                        ) {
-                            Text(category.text)
-                        }
-//                LazyRow {
-//                    items(Category.entries) {
-//                        Button({},
-//                            Modifier.padding(4.dp),
-//                            colors = ButtonDefaults.buttonColors(it.color, it.textColor)
-//                        ) { Text(it.string) }
+        MultiChoiceSegmentedButtonRow(Modifier.horizontalScroll(ScrollState(0))) {
+            for (category in Category.entries) {
+                CategoryButton(category) {
+                    if (it) {
+                        categories += category.name
+                    } else {
+                        categories -= category.name
                     }
                 }
             }
+        }
+        LazyColumn(Modifier.weight(1f)) {
             items(posts) {
                 MeetingEntry.FromEntity(it)
             }
